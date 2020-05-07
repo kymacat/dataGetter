a68b92557297e017fc9ca7431f0a2bfb95100da3
Vlad Yandola <vladan2000.9@mail.ru>
2020-05-07 21:41:16 +0500

    Initial Commit

---
+++ dataGetter/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  dataGetter
+//
+//  Created by Const. on 07.05.2020.
+//  Copyright © 2020 Oleginc. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ dataGetter/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  dataGetter
+//
+//  Created by Const. on 07.05.2020.
+//  Copyright © 2020 Oleginc. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
